ac825efb37ff967453a67e84774789500df7bc9f
ernarku <ernarku@freedombank.kz>
2024-10-21 16:32:27 +0500

    initial commit

---
+++ app/src/main/java/com/example/wishlistapp/AppBar.kt
@@ -0,0 +1,23 @@
+package com.example.wishlistapp
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    title: String,
+    onBackClick: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(text = title)
+        },
+        navigationIcon = {
+
+        }
+
+    )
+}
---
+++ app/src/main/java/com/example/wishlistapp/HomeScreen.kt
@@ -0,0 +1,25 @@
+package com.example.wishlistapp
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(){
+    Scaffold(
+        topBar = {
+            AppBar(title = "") {
+                
+            }
+        }
+    ) {
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)) {
+
+        }
+    }
+}
