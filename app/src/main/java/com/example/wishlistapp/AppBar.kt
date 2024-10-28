5761f42108c1ef062df0cd35f3bf494ad21e48a7
Kulmanov <200103127@stu.sdu.edu.kz>
2024-10-28 12:37:50 +0500

    Added FAB

--- app/src/main/java/com/example/wishlistapp/AppBar.kt
+++ app/src/main/java/com/example/wishlistapp/AppBar.kt
@@ -1,16 +1,27 @@
 package com.example.wishlistapp
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -18,23 +29,30 @@ fun AppBar(
     title: String,
     onBackClick: () -> Unit
 ) {
+    val navigationIcon: (@Composable () -> Unit) = {
+        IconButton(onClick = onBackClick) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
+    }
+
     TopAppBar(
         modifier = Modifier
-            .padding(start = 4.dp)
-            .heightIn(max = 24.dp)
-            .background(
-                color = colorResource(
-                    id = R.color.app_bar_color
-                )
-            ),
+            .fillMaxWidth(),
         title = {
             Text(
                 text = title,
-                color = colorResource(id = R.color.white)
+                color = Color.White,
+                fontSize = 18.sp,
+                lineHeight = 22.sp
             )
         },
-        navigationIcon = {
-
-        }
+        colors = TopAppBarDefaults.topAppBarColors().copy(containerColor = colorResource(
+            id = R.color.app_bar_color
+        )),
+        navigationIcon = navigationIcon,
     )
 }
--- app/src/main/java/com/example/wishlistapp/HomeScreen.kt
+++ app/src/main/java/com/example/wishlistapp/HomeScreen.kt
@@ -1,24 +1,61 @@
 package com.example.wishlistapp
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
+    val context = LocalContext.current
     Scaffold(
+        modifier = Modifier.padding(WindowInsets.statusBars.asPaddingValues()),
         topBar = {
-            AppBar(title = "") {
-                
+            Column(modifier = Modifier
+                .fillMaxWidth().height(80.dp),
+                verticalArrangement = Arrangement.Center) {
+                AppBar(title = "WishList") {
+
+                }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                containerColor = Color.Black,
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null
+                )
             }
         }
     ) {
-        LazyColumn(modifier = Modifier
-            .fillMaxSize()
-            .padding(it)) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
 
         }
     }
